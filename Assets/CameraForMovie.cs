57d5ced5f5173a5598ada992d7ab89b0a4a308b7
Thifer <buybackbrain.genetic@gmail.com>
2020-10-06 19:41:58 +0300

    -temp

--- Assets/CameraForMovie.cs
+++ Assets/CameraForMovie.cs
@@ -8,16 +8,44 @@ using UnityEngine.PlayerLoop;
 
 public class CameraForMovie : MonoBehaviour
 {
+    public enum act
+    {
+        look,
+        rotate,
+    }
+    
+    
+    [Serializable]public struct doing
+    {
+        public float starttime;
+        public float endingtime;
+        public act act;
+        public GameObject prefab;
+        public GameObject prefabs { get=>prefab;
+            set => prefab = value;
+        }
+        public float angel;
+        public Vector3 distace;
+        public Vector3 axis;
+    }
+    
+    
     List<IEnemy> enemies = new List<IEnemy>();
     public GameObject Player;
     public float distance;
     public bool FindPlayer;
     public Vector3 Range;
-    public GameObject turret1;
-    public GameObject turret2;
-    public GameObject turret3;
-    public GameObject turret4;
     public List<GameObject> point = new List<GameObject>();
+    public List<doing> things;
+    public float currenttime;
+    public bool starting;
+    public int num;
+    private float offsetx;
+    private float offsety;
+    private GameObject rotatearound;
+    private Queue<doing> _doings = new Queue<doing>();
+    private doing current;
+    private bool issetdistance;
 
 
     private void Update()
@@ -28,45 +56,102 @@ public class CameraForMovie : MonoBehaviour
             if (Player != null)
             {
                 SetCamera();
-                point.AddRange(GameObject.FindGameObjectsWithTag("TurretPoint"));
+                starting = true;
+                fillgamepref();
+                
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha1) && turret1 == null)
+        if (starting)
         {
-            turret1 = Instantiate(Data.Instance.TurretData.CannonTurret.TurretPrefab, Player.transform);
-            turret1.transform.position = point[1].transform.position;
+            Debug.Log(current.act);
+            currenttime += Time.deltaTime;
+            if (currenttime > _doings.Peek().starttime)
+            {
+                issetdistance = false;
+                current = _doings.Dequeue();
+            }
 
-        }
+            if (currenttime <current.endingtime )
+            {
+                
+                startnext(current);
+            }
 
-        if (Input.GetKeyDown(KeyCode.Alpha2) && turret2 == null)
-        {
-            turret2 = Instantiate(Data.Instance.TurretData.FrostTurret.TurretPrefab, Player.transform);
-            turret2.transform.position = point[2].transform.position;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha3) && turret3 == null)
-        {
-            turret3 = Instantiate(Data.Instance.TurretData.GrenadeTurret.TurretPrefab, Player.transform);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha4) && turret4 == null)
-        {
-            turret4 = Instantiate(Data.Instance.TurretData.LaserTurret.TurretPrefab, Player.transform);
+            if (currenttime >current.endingtime  )
+            {
+                SetCamera();
+            }
         }
     }
+
+
+    private void fillgamepref()
+    {
+
+        things.Sort((doing, doing1) => doing.starttime>doing1.starttime? 1:0 );
+        
+
+        for (int i = 0; i < things.Count; i++)
+        {
+            _doings.Enqueue(things[i]);
+        }
+        Debug.Log(_doings.Count);
+    }
+
+
+    private void startnext(doing a)
+    {
+        switch (a.act)
+        {
+            case act.look:
+                look(a);
+                break;
+            case act.rotate:
+                rotate(a);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+
+    private void rotate(doing a)
+    {
+        if (!issetdistance)
+        {
+            gameObject.transform.SetParent(a.prefab.transform);
+            transform.position = a.distace;
+            issetdistance = true;
+        }
+        transform.RotateAround(a.prefabs.transform.position,a.axis,a.angel*Time.deltaTime);
+        //transform.LookAt(a.prefab.transform);
+    }
+
+    private void look(doing a)
+    {
+        if (!issetdistance)
+        {
+            transform.position = a.distace;
+            issetdistance = true;
+            transform.LookAt(Player.transform);
+        }
+    }
+    
+    private void resetCamera()
+    {
+        gameObject.transform.SetParent(GameObject.FindGameObjectWithTag("TurretPoint").transform);
+        rotatearound = GameObject.FindGameObjectWithTag("TurretPoint");
+        transform.rotation = Quaternion.identity;
+        transform.position = new Vector3(0, 5, -5);
+        transform.LookAt(rotatearound.transform);
+    }
+
 
     private void SetCamera()
     {
         transform.position = Range;
         gameObject.transform.SetParent(Player.transform);
         transform.LookAt(Player.transform);
-    }
-
-    private void UpdateTurret()
-    {
-        enemies.Clear();
-        enemies.AddRange(Services.Instance.LevelService.ActiveEnemies);
-        
     }
 }
--- Assets/Scripts/Controllers/TurretController.cs
+++ Assets/Scripts/Controllers/TurretController.cs
@@ -24,10 +24,10 @@ namespace Snake_box
         {
             _hiddenInstance = this;
 
-            Data.Instance.TurretData._turretQueue.Enqueue(Data.Instance.TurretData.TurretPlant.AddCannonTurret());
-            Data.Instance.TurretData._turretQueue.Enqueue(Data.Instance.TurretData.TurretPlant.AddFrostTurret());
-            Data.Instance.TurretData._turretQueue.Enqueue(Data.Instance.TurretData.TurretPlant.AddLaserTurret());
-            Data.Instance.TurretData._turretQueue.Enqueue(Data.Instance.TurretData.TurretPlant.AddShotgunTurret());
+            //Data.Instance.TurretData._turretQueue.Enqueue(Data.Instance.TurretData.TurretPlant.AddCannonTurret());
+            //Data.Instance.TurretData._turretQueue.Enqueue(Data.Instance.TurretData.TurretPlant.AddFrostTurret());
+            //Data.Instance.TurretData._turretQueue.Enqueue(Data.Instance.TurretData.TurretPlant.AddLaserTurret());
+            //Data.Instance.TurretData._turretQueue.Enqueue(Data.Instance.TurretData.TurretPlant.AddShotgunTurret());
         }
 
         #endregion
@@ -43,10 +43,10 @@ namespace Snake_box
 
             _turretList.ForEach(iExecutable => iExecutable.Execute());
 
-//            if (new InputService().IsKeysPressed())
-//            {
-//                Data.Instance.TurretData.TurretPlant.ChangeTurretType(new InputService().KeyDownIs());
-//            }
+            if (new InputService().IsKeysPressed())
+            {
+                Data.Instance.TurretData.TurretPlant.ChangeTurretType(new InputService().KeyDownIs());
+            }
         }
 
         #endregion
